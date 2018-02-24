9d8b6144a6a1e44abe0bbaa1bb5069fd540183de
Patrick Grady <patrickgrady80@gmail.com>
2018-02-24 13:13:31 -0500

    Made minor changes to globals, right now the script launches a test that populates 51 entries for today, so can begin working on daily analyzer. Test data is included

--- fda_foodapi.py
+++ fda_foodapi.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import csv
+import os
 from datetime import date
 import config
 
@@ -10,16 +11,13 @@ from globals import NUTRIENT_IDS as nu
 
 def userSetup():
     name = input("What is your name: ")
-    response = int(input("Is this your first time using the program input 1 for no or 2 for yes: "))
+    response = int(input("Is this your first time using the program input 1 for yes or 2 for no: "))
     if response is 1:
         return
     elif response is 2:
-        with open('{}_data.csv'.format(name), 'w+') as csvfile:
-            write = csv.writer(csvfile)
-            write.writerow(HEADERS)
-    else:
-        print("Please enter valid input")
-        userSetup()
+        filename = '{}_data.csv'.format(name)
+        
+    return filename
 
 def foodSearch():
     searchTerm = input("Enter the food you want to search for: ").split()
@@ -56,7 +54,7 @@ def getNutrition(data):
     return nutr_data
     
 def parseNutrition(nutr_data):
-    user_date = input("Enter the date for this entry in mm-dd-yyyy format or enter today: ")
+    '''user_date = input("Enter the date for this entry in mm-dd-yyyy format or enter today: ")
     #This breaks hopelessly if the user enters bad data, no idea why
     try:
         if user_date == 'today':
@@ -66,8 +64,8 @@ def parseNutrition(nutr_data):
             user_date = date(year, month, day).strftime('%m-%d-%Y')
     except ValueError:
         print('Enter a valid format')
-        parseNutrition(nutr_data)
-           
+        parseNutrition(nutr_data)'''
+    user_date = date.today().strftime('%m-%d-%Y')
     nutrparse = nutr_data
     food_name = nutrparse['report']['foods'][0]['name']
     serving_size = nutrparse['report']['foods'][0]['measure']
@@ -86,28 +84,42 @@ def parseNutrition(nutr_data):
             fat = i['value']
         elif i['nutrient_id'] == '205':
             carbs = i['value']
-    keys = ['date','food_name', 'serving_size','calories','fiber','protein','sugars','fat','carbs']
+    keys = HEADERS
     values = [user_date,food_name,serving_size,calories,fiber,protein,sugars,fat,carbs]
     nutr_dict = dict(zip(keys,values))
     return nutr_dict
 
-def test():
+def recordResults(file_name, results):
+    file_exist = os.path.isfile(file_name)
+    with open(file_name, 'a+', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=HEADERS)
+        if not file_exist:
+            writer.writeheader()
+        writer.writerow(results)
+
+
+def test(item):
     search_url = 'https://api.nal.usda.gov/ndb/search/?format=json&q={0}&sort=n&max={1}&offset=0&api_key={2}'.format('Mcdonald', '60', config.api_key)
     user_file = 'Patrick_data.csv'
     nutr_url = ('https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key={0}&nutrients='
                 '{1}&nutrients={2}&nutrients={3}&nutrients={4}&nutrients={5}&nutrients={6}&ndbno={7}'.format(config.api_key, nu['kcal'], 
-                nu['fat'], nu['protein'], nu['carbs'], nu['fiber'], nu['sugars'], '21228'))
+                nu['fat'], nu['protein'], nu['carbs'], nu['fiber'], nu['sugars'], item))
     response = requests.get(nutr_url)
     nutr_data = json.loads(response.text)
+    results = parseNutrition(nutr_data)
     return nutr_data
 
-def main():    
-    nutr_data = test()
-    results = parseNutrition(nutr_data)
-    print(results)
-
+def main_test():
+    filename = 'Patrick_data.csv'
+    with open('testdata.txt','r') as testdata:
+        item_list = testdata.read()
+        item_list = item_list.split('\n')
+    for item in item_list:
+        nutr_data = test(item)
+        results = parseNutrition(nutr_data)
+        recordResults(filename, results)
 
     
 
 
-main()
+main_test()
--- globals.py
+++ globals.py
@@ -1,2 +1,2 @@
-HEADERS = ['Date', 'Food Name', 'Serving Size', 'Calories', 'Fat', 'Protein', 'Carbohydrates']
+HEADERS = ['date', 'food_name', 'serving_size', 'calories', 'fiber','protein','sugars','fat','carbs']
 NUTRIENT_IDS = {'kcal': '208', 'protein': '203', 'fat':'204', 'carbs': '205', 'fiber': '291', 'sugars': '269'}
